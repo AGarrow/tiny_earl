3a078882555d1508e093ecdee64bfbfc12e9146e
AGarrow <alexio2@mac.com>
2017-10-14 16:25:24 -0400

    routes: earl resource as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :earls, only: %i[index show create], param: :short_url
+  resources :earls, only: %i[index show create], param: :short_url, path: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'earls#index'
 end
